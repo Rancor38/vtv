7f8b2d4aedb1e4d5bcc2af40db01675562776cfd
Rancor38 <zak.rancor@hotmail.com>
2026-01-29 17:31:07 -0800

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/vtv/',
   plugins: [react()],
-  base: '/vtv/', // Replace 'vtv' with your GitHub repo name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
